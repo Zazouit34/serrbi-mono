c368c6898e9e77be70cfd63a49181b6a414bdc2b
Zazouit34 <zazouitmed@gmail.com>
2025-08-23 00:01:59 +0100

    light theme 34

--- apps/web/components/providers.tsx
+++ apps/web/components/providers.tsx
@@ -3,18 +3,17 @@
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 
-
-
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NextThemesProvider
       attribute="class"
-      forcedTheme="light"      // always light
-      enableSystem={false}     // ignore OS theme
+      defaultTheme="light"
+      forcedTheme="light"
+      enableSystem={false}
       disableTransitionOnChange
+      storageKey="theme"  // optional, resets any old key names
     >
       {children}
     </NextThemesProvider>
-  
   )
 }
--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,6 +1,7 @@
 // tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     "./node_modules/@heroui/theme/dist/components/(button|card|ripple|spinner).js",
   ],
