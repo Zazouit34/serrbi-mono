20d2adbfdd98d2126e308b93dfc338d648ab2003
Zazouit34 <zazouitmed@gmail.com>
2025-08-22 23:26:11 +0100

    add force dynamic to login and register

--- apps/web/app/(auth)/login/page.tsx
+++ apps/web/app/(auth)/login/page.tsx
@@ -1,4 +1,5 @@
 import LoginForm from "@/components/ui/form/login-form";
+export const dynamic = "force-dynamic"
 
 export default function LoginPage() {
   return <LoginForm />;
--- apps/web/app/(auth)/register/page.tsx
+++ apps/web/app/(auth)/register/page.tsx
@@ -1,4 +1,5 @@
 import RegisterForm from "@/components/ui/form/register-form";
+export const dynamic = "force-dynamic"
 
 export default function RegisterPage() {
   return <RegisterForm />;
