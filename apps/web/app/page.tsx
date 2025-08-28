cf6ea05eaedfb6e99f42e434ab37402ac499203a
Serrbi/Acc <skymark98@gmail.com>
2025-08-28 14:34:56 +0100

    fix the run build

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -14,7 +14,7 @@ export default function Page() {
   return (
     <main className="flex relative flex-col mx-auto">
       <h1>
-        this is the main page as statically generated
+        this is the main page 
       </h1>
     </main>
   )
