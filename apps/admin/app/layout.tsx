19f01b0d9a4516c68e3aff65a88a1c54834fa5e9
Serrbi/Acc <skymark98@gmail.com>
2025-09-23 13:50:53 +0100

    add the tailwind config to admin

--- apps/admin/app/layout.tsx
+++ apps/admin/app/layout.tsx
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 
-import "./global.css"; 
+import "./global.css" 
 import { Toaster } from "@workspace/ui/components/sonner";
 import Provider from "@/app/_trpc/provider";
 import { SessionProvider } from "next-auth/react";
