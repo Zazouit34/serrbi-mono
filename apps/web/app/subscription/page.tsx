851f9382832662a6a816b13e008e12f380859ec3
Serrbi/Acc <skymark98@gmail.com>
2025-09-30 16:00:51 +0100

    fix the push to vercel

--- apps/web/app/subscription/page.tsx
+++ apps/web/app/subscription/page.tsx
@@ -1,5 +1,8 @@
 import SubscriptionPageClient from "../../components/ui/subscription/subscription-page-client";
 
+export const dynamic = 'force-dynamic';
+
+
 export default function SubscriptionPage() {
   return <SubscriptionPageClient />;
 }
--- apps/web/app/subscription/success/page.tsx
+++ apps/web/app/subscription/success/page.tsx
@@ -1,5 +1,8 @@
 import SubscriptionSuccessClient from "../../../components/ui/subscription/subscription-success-client";
 
+export const dynamic = 'force-dynamic';
+
+
 export default function SubscriptionSuccessPage() {
   return <SubscriptionSuccessClient />;
 }
