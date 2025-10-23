645cfe7afb75e42e0db6f3c7bb52f1a99af4075d
Serrbi/Acc <skymark98@gmail.com>
2025-10-23 02:26:51 +0100

    change look of price on job-card

--- apps/web/components/ui/form/markdown-render.client.tsx
+++ apps/web/components/ui/form/markdown-render.client.tsx
@@ -3,13 +3,17 @@
 import { cn } from "@workspace/ui/lib/utils"
 import ReactMarkdown from "react-markdown"
 import remarkGfm from "remark-gfm"
+import rehypeRaw from "rehype-raw"
+import rehypeSanitize from "rehype-sanitize"
 
 export const markdownClassNames = "max-w-none prose prose-neutral font-sans"
 
 export function MarkdownRendererClient({ className, source }: { className?: string; source: string }) {
   return (
     <div className={cn(markdownClassNames, className)}>
-      <ReactMarkdown remarkPlugins={[remarkGfm]}>{source}</ReactMarkdown>
+      <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw, rehypeSanitize]}>
+        {source}
+      </ReactMarkdown>
     </div>
   )
 }
