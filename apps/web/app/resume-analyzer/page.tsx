bf4c891fea217cefb7425c85a282a59cf2e785f8
Serrbi/Acc <skymark98@gmail.com>
2026-01-01 17:45:14 +0100

    second look on Resume analyzer

--- apps/web/app/resume-analyzer/page.tsx
+++ apps/web/app/resume-analyzer/page.tsx
@@ -4,12 +4,7 @@ import { getTranslations } from "next-intl/server";
 
 export default async function ResumeAnalyzerPage() {
   await requireUser("/resume-analyzer");
-  const t = await getTranslations("ResumeAnalyzerPage");
+  
 
-  return (
-    <div className="flex flex-col gap-10 py-16 mx-auto">
-      <ResumeInsight requireLogin callbackUrl="/resume-analyzer" />
-    </div>
-  );
+  return <ResumeInsight requireLogin callbackUrl="/resume-analyzer" />;
 }
-
